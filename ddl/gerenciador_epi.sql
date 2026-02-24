6ae461c33ecff9f132b74f3c46dae1368819e258
Desperaux7 <gustavomuller2105@gmail.com>
2026-02-23 22:50:48 -0300

    feature-entidade-tipo_epi-tamanho-estoque
    Adicionando entidades: tipo_epi; tamanho; estoque

--- ddl/gerenciador_epi.sql
+++ ddl/gerenciador_epi.sql
@@ -21,3 +21,22 @@ nome_setor varchar(100),
 descricao varchar(150),
 constraint pk_setor primary key (id_setor)
 );
+
+create table tipo_epi (
+    id_tipo_epi INT PRIMARY KEY,
+    nome_epi VARCHAR(100),
+    ca VARCHAR(20),
+    dias_vida_util INT,
+    instrucoes_uso TEXT
+);
+
+create table tamanho (
+    id_tamanho INT PRIMARY KEY,
+    descricao VARCHAR(50)
+);
+
+create table estoque (
+    id_estoque INT PRIMARY KEY,
+    quantidade_disponivel INT,
+    quantidade_minima INT
+);
